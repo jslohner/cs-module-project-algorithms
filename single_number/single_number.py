4628db6aef44ab2c283b4a0f181cee7e122399ba
Justin Lohner <lohner4@gmail.com>
2020-07-22 12:34:15 -0600

    wrote single_number function

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,13 +3,27 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
-
+	# create memory storage
+	mem = {}
+	# loop through array
+	for num in arr:
+		# if num is already in
+		# mem add 1 to it's value
+		if num in mem:
+			mem[num] += 1
+		# otherwise initialize number
+		# with value of 1
+		else:
+			mem[num] = 1
+	# loop through mem
+	for key in mem:
+		# if key has value of 1
+		# return that key
+		if mem[key] == 1:
+			return key
 
 if __name__ == '__main__':
-    # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+	# Use the main function to test your implementation
+	arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
-    print(f"The odd-number-out is {single_number(arr)}")
+	print(f"The odd-number-out is {single_number(arr)}")
