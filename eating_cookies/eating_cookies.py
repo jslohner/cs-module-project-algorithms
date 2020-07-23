df89b5f8742588d69ec50af7d6ba958e9311f158
Justin Lohner <lohner4@gmail.com>
2020-07-23 14:45:42 -0600

    wrote eating_cookies program

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,21 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
-
-    pass
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+def eating_cookies(n, mem):
+	if n < 0:
+		return 0
+	elif n == 0:
+		return 1
+	elif n in mem:
+		return mem[n]
+	else:
+		mem[n] = eating_cookies((n - 1), mem) + eating_cookies((n - 2), mem) + eating_cookies((n - 3), mem)
+		return mem[n]
 
 if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
+	# Use the main function here to test out your implementation
+	num_cookies = 500
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+	print(f"There are {eating_cookies(num_cookies, {})} ways for Cookie Monster to eat {num_cookies} cookies")
